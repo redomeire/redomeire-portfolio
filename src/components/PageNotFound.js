7557bf1c212c0453278e47b3b4cefe4ccbf0b24c
Redomeire <redomeire2@gmail.com>
2022-07-05 14:27:14 +0700

    change MUI link to react router dom link

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,4 +1,5 @@
-import { Link, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 import PageNotFoundImage from "../assets/page_not_found.svg";
 import UnderConstructionImage from "../assets/under_construction.svg";
 import CustomButton from "./CustomButton";
@@ -10,7 +11,7 @@ function PageNotFound(props){
             <img src={props?.value === "not found" ? PageNotFoundImage : UnderConstructionImage} alt="hero" width="300" style={{marginBottom: "20px"}}/>
             <Typography variant="h4" sx={{marginBottom: "10px", fontWeight: "700", fontFamily: "Source Sans Pro"}}>Oops...</Typography>
             <Typography variant="h6" sx={{marginBottom: "20px", fontFamily: "Source Sans Pro"}}>{props?.value === "not found" ? "There is nothing here" : "website under construction"}</Typography>
-            <Link href="/" sx={{textDecoration: "none"}}>
+            <Link to="/" style={{textDecoration: "none"}}>
                 <CustomButton startIcon={<HomeIcon/>} variant="outlined" style={{color: "#21A099", borderColor: "#21A099",fontFamily: "Source Sans Pro",textTransform: "capitalize", padding: "10px"}}>Go to Home</CustomButton>
             </Link>
         </div>
