0bc32621ac7678969fad2e3f299f887362010fea
Redomeire <redomeire2@gmail.com>
2022-08-24 00:55:43 +0700

    sync with imagekit

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,5 +19,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore();
+const colRef = collection(db, 'projects');
 
-export { analytics, db };
+export { analytics, db, colRef };
