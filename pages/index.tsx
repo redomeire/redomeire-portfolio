2e1b6532977f0a2b1ed40e4df1f91f121aec8e85
Redomeire <redomeire2@gmail.com>
2023-05-13 01:44:13 +0700

    migrate project to next@12

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import { Button } from "@mui/material";
+
+const Home = () => {
+    return ( 
+        <div>
+            <Button>click</Button>
+        </div>
+     );
+}
+ 
+export default Home;
---
+++ pages/profile/index.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+const experienceData = [
+    {
+        id: 1,
+        title: "Member of Front End Department",
+        place: "BCC Fakultas Ilmu Komputer Universitas Brawijaya",
+        body: "BCC FILKOM is a tech-based community in Faculty of Computer Science, University of Brawijaya. BCC consist of 4 departments, they are Product, Front-End, Back-End, and Computational Intelligence.",
+        duration: "Mar 2022 - Now"
+    },
+    {
+        id: 2,
+        title: "Staff Muda Research and Development",
+        place: "Eksekutif Mahasiswa TI",
+        body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        duration: "Oct 2021 - Jan 2022"
+    },
+    {
+        id: 3,
+        title: "Wakil Koordinator Desain Web",
+        place: "Ambis UTBK",
+        body: "I responsible to make design and developing the website I responsible to make design and developing the website",
+        duration: "Aug 2021 - Now"
+    }
+];
+
+function Profile() {    
+    return (
+        <div>
+            home
+        </div>
+    );
+}
+
+export default Profile;
