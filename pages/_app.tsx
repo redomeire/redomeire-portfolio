9c9cd6b8d3fa03d9b180b0f736cdc51282491d3a
Redomeire <redomeire2@gmail.com>
2023-05-15 07:05:48 +0700

    fix eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1"
-        ></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       </Head>
       <Component {...pageProps} />
     </>
